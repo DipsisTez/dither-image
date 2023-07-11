1bcbbb6b14953ec4ba9e2a25d37936082fa5ca7a
DipsisTez <pandoradipsis@mail.ru>
2023-07-11 22:09:19 +0700

    made visual changes on the about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
 import {strings} from './strings';
+import './style.css';
 
 const About = () => {
   return (
-    <div>
-      <h2>{strings.aboutDescription}</h2>
+    <div className="Box">
+      <h2 className="Title">{strings.aboutDescription}</h2>
+      <p className="Description">{strings.distDescription}</p>
     </div>
   );
 };
--- src/pages/About/strings.ts
+++ src/pages/About/strings.ts
@@ -1,3 +1,4 @@
 export const strings = {
   aboutDescription: 'About page',
+  distDescription: 'This application is a simple image processing tool.',
 };
